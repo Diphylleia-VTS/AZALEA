129a46478b5a499fe76db2369e4c0c2a8aa07cde
Charlotte_Jeremiah <40084865+lelelove1225@users.noreply.github.com>
2021-12-16 14:36:06 +0900

    push for public

--- src/components/organisims/HeaderComponent/HeaderComponent.tsx
+++ src/components/organisims/HeaderComponent/HeaderComponent.tsx
@@ -3,13 +3,14 @@ import { HeroLogo } from "../../atoms/HeroLogo/HeroLogo";
 import HeaderLinks from "../../molecules/HeaderLinks/HeaderLinks";
 import { makeStyles } from "@material-ui/core/styles";
 
-const backgroundColor = "#64121D";
+const backgroundColor = "#6D120F";
 
 const useStyles = makeStyles(() => ({
   root: {
     backgroundColor: backgroundColor,
     width: "100%",
-    paddingTop: "1vh",
+    paddingTop: "1.5vh",
+    paddingBottom: "1vh"
   },
 }));
 
@@ -18,7 +19,6 @@ export const HeaderComponent: FunctionComponent = () => {
   return (
     <div className={classes.root}>
       <HeroLogo />
-      <HeaderLinks />
     </div>
   );
 };
