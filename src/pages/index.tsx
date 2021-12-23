71262f0f0d9cad6f3e742d3b8ae4bf6b6fe14ec5
Charlotte_Jeremiah <40084865+lelelove1225@users.noreply.github.com>
2021-12-24 01:16:50 +0900

    ogp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,7 +59,7 @@ const Home: FunctionComponent = () => {
         <meta name="twitter:site" content="@Diphyl_Grayi" />
         <meta name="twitter:title" content="Diphylleia" />
         <meta name="twitter:description" content="Next fall, bloom❁" />
-        <meta property="og:image" content="/preview.png" />
+        <meta property="og:image" content="diphylleia-grayi.com/assets/preview.png" />
         <meta name="twitter:card" content="summary_large_image"/>
       </Head>
       <div className={classes.background}>
